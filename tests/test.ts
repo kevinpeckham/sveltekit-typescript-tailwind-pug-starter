239a8346869c6e8a6b522dfe2854d2122f052055
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2022-09-23 10:03:35 -0400

    bug: fixed example in test.ts

--- tests/test.ts
+++ tests/test.ts
@@ -2,7 +2,6 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	expect(await page.textContent('h1')).toBe(
-		'Welcome to The SvelteKit Typescript Tailwind Pug Starter.'
-	);
+	const h1 = 'Welcome to the SvelteKit Typescript Tailwind Pug Starter.';
+	await expect(page.locator('h1')).toHaveText(h1);
 });
